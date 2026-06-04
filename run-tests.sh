80fc2f61d89eed6a80bcf3dd8806209bdf67219f
CDasse <clemence.dasse@yahoo.fr>
2026-06-04 16:31:08 +0200

    Feat: create a script to cleanup the database fixture before and after lunching tests

---
+++ run-tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#--------------------------------------------------
+# Name : run-tests.sh
+# Author : CDasse
+# Purpose : Cleanup the database fixture before
+#           and after lunching tests
+# Usage : ./run-tests.sh
+#--------------------------------------------------
+
+set -euo pipefail
+
+echo "[1/3] DataBase reinitialisation"
+symfony console doctrine:fixtures:load
+
+echo "[2/3] Run tests"
+npx playwright test
+
+echo "[3/3] DataBase reinitialisation"
+symfony console doctrine:fixtures:load
